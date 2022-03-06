cef2fa25d933f76c7ce5bf15ec100d4b697c429d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-06 14:56:57 +0530

    Upload screenshot as artifact

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 //  Library
 import * as core from '@actions/core'
+import * as artifact from '@actions/artifact'
 import { captureScreenshot } from './captureScreenshot'
 import { url, name } from './library'
 
@@ -11,6 +12,11 @@ import { url, name } from './library'
 async function run() {
     try {
         await captureScreenshot(url, name)
+        const client = artifact.create()
+        const artifactName = name.substring(0, name.lastIndexOf('.'))
+        const upload = await client.uploadArtifact(artifactName, [`./${name}.png`], './')
+
+        core.setOutput('path', `./${name}.png`)
     } catch (err) {
         let error = err as Error
         core.setFailed(error.message)
