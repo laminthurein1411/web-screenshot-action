b2942707eb2a12b48cf9bf2f522a77b265e3355b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-07 16:53:02 +0530

    Add comments to captureScreenshot.ts

--- src/captureScreenshot.ts
+++ src/captureScreenshot.ts
@@ -9,24 +9,31 @@ type captureOptions = {
     height?: number
 }
 
+/** Capture screenshot of the given URL */
 export async function captureScreenshot(url: string, name: string, options?: captureOptions) {
 
+    //  Get options
     const width = options?.width || 1920
     const height = options?.height || 1080
 
+    //  Launch browser with the provided settings
     const browser = await puppeteer.launch({
         executablePath: getChromePath(),
         defaultViewport: { height, width }
     })
 
+    //  Navigate to the given URL
     const page = await browser.newPage()
     await page.goto(url, {
         waitUntil: 'networkidle2'
     })
 
+    //  Take screenshot of the webpage and save it as a PNG
     await page.screenshot({
         path: `${process.env.GITHUB_WORKSPACE}/${name}.png`
     })
 
+    //  Close the browser
     await browser.close()
+
 }
