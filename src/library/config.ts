165b986b69943a87738c48b465c772b9e8cf0a54
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-07 16:41:53 +0530

    Accept width and height as input parameters

--- src/library/config.ts
+++ src/library/config.ts
@@ -5,8 +5,16 @@ import * as core from '@actions/core'
 //  CONFIG
 //  ======
 
+/** URL to take screenshot of */
 export const url: string = core.getInput('url', { required: true })
 
-export const name: string = core.getInput('name') || 'screenshot.png'
+/** Screenshot width */
+export const width: number = parseInt(core.getInput('width'))
+/** Screenshot height */
+export const height: number = parseInt(core.getInput('height'))
 
+/** Screenshot fileName */
+export const name: string = core.getInput('name')
+
+/** Boolean flag to determine if the action generates artifacts */
 export const createArtifacts: boolean = core.getBooleanInput('createArtifacts')
