f59053de0c7e719d8060d210cdb48f4eb2bc6ef1
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-04-02 13:27:55 +0530

    Extract inputs into metadata

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 //  Library
 import * as core from '@actions/core'
+import { inputs } from './metadata'
 
 //  Type Definitions
 import type { ScreenshotOptions } from 'puppeteer-core'
@@ -9,31 +10,31 @@ import type { ScreenshotOptions } from 'puppeteer-core'
 //  ======
 
 /** URL to take screenshot of */
-export const url: string = core.getInput('url', { required: true })
+export const url: string = core.getInput(inputs.url, { required: true })
 if (!url) { throw new Error('URL is required!') }
 
 /** Screenshot fileName */
-export const path: string = core.getInput('path')
+export const path: string = core.getInput(inputs.path)
 
 /** Screenshot width */
-export const width: number = parseInt(core.getInput('width'))
+export const width: number = parseInt(core.getInput(inputs.width))
 /** Screenshot height */
-export const height: number = parseInt(core.getInput('height'))
+export const height: number = parseInt(core.getInput(inputs.height))
 /** Should take screenshot of the entire page */
-export const captureFullPage = core.getBooleanInput('captureFullPage')
+export const captureFullPage = core.getBooleanInput(inputs.captureFullPage)
 /** Should capture beyond viewport */
-export const captureBeyondViewport = core.getBooleanInput('captureBeyondViewport')
+export const captureBeyondViewport = core.getBooleanInput(inputs.captureBeyondViewport)
 /** Should omit the background */
-export const omitBackground = core.getBooleanInput('omitBackground')
+export const omitBackground = core.getBooleanInput(inputs.omitBackground)
 /** encoding */
-export const encoding = core.getInput('encoding') as ScreenshotOptions['encoding']
+export const encoding = core.getInput(inputs.encoding) as ScreenshotOptions['encoding']
 
 
 /** Boolean flag to determine if the action generates artifacts */
-export const shouldCreateArtifacts: boolean = core.getBooleanInput('shouldCreateArtifacts')
+export const shouldCreateArtifacts: boolean = core.getBooleanInput(inputs.shouldCreateArtifacts)
 
 /** Time to wait before taking screenshot */
-export const delay: number = +core.getInput('delay')
+export const delay: number = +core.getInput(inputs.delay)
 
 /** Prefers Dark Color Scheme */
-export const darkMode: boolean = core.getBooleanInput('darkMode')
+export const darkMode: boolean = core.getBooleanInput(inputs.darkMode)
---
+++ src/metadata.ts
@@ -0,0 +1,26 @@
+//  ===============
+//  ACTION METADATA
+//  ===============
+
+//  ========
+//  METADATA
+//  ========
+
+/** Metadata inputs */
+export const inputs = {
+    url: 'url',
+    path: 'path',
+    width: 'width',
+    height: 'height',
+    captureFullPage: 'captureFullPage',
+    captureBeyondViewport: 'captureBeyondViewport',
+    omitBackground: 'omitBackground',
+    encoding: 'encoding',
+    shouldCreateArtifacts: 'shouldCreateArtifacts',
+    delay: 'delay',
+    darkMode: 'darkMode'
+} as const
+
+/** Metadata outputs */
+export const outputs = {
+} as const
